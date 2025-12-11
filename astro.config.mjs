75e0df848055c55a4f5458768e3c6b7c7b75282f
yamiddevofic <yhrodriguez1@hotmail.com>
2025-12-11 00:10:41 -0500

    Fix OG: dominio correcto y nombre de imagen

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://evelyncosmetics.com', // Cambiar por tu dominio real
+  site: 'https://evelyncosmeticsbol.vercel.app',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
